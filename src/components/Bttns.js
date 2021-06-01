b34710191dc39407b8d408468ec100ffe2c50e35
IlyannaYates <ilyanna.yates@gmail.com>
2021-05-31 23:41:29 -0700

    update

---
+++ src/components/Bttns.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+
+function Bttns ({tech}){
+
+    return(
+        <>
+        <span className="tech">{tech}</span>
+        </>
+    )
+
+}
+
+export default Bttns; 
